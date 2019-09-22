633b9d7a8503a42cc3f5d16a29cad2a6ddf0e978
myns <myunusyilmaz@gmail.com>
2019-09-22 23:53:58 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View,Button,TextInput } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText]= useState("düşmeemmeme");
   return (
-    <View style={styles.container}>
+    <View style={{padding:50}}>
+
+      <View>
+<TextInput placeholder="AMAÇIMIZ"/>
+
+<Button title="bas bana baby"/>
+
+      </View>
+      <View>
       <Text>{outputText}</Text>
-      <Button title = "bas bas bas 2" onPress={()=>setOutputText("bastuj vala") }></Button>
+      <Text>deneme 343343</Text>
+      <Button title = "bas bas bas 24" onPress={()=>setOutputText("bastuj vala") }></Button>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
